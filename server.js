0086c24d042452e14f092c7ec2924ae010077d06
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-05-21 22:25:19 +0100

    set up the maintenance.hbs file and rendered the page in server.js. This code is used as an example of how maintenance can be carried out on a site by only displaying the maintenance message to the user. Have commented out the code on server.js so the app can still be run. Also used the node module fs to create the server.log file and log the timestamp to the file each time the user requests the web page from the browser.

--- server.js
+++ server.js
@@ -8,19 +8,47 @@
 
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 
+//middleware function
+//have to call next in order for the rest of the functions to execute.
+//next signals that the action is complete and the app can continue executing
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  //when the user requests the we page, the date is timestamped to the terminal
+  //the request.method is an express api method. https://expressjs.com/en/4x/api.html#req.ips
+  var log = `${now}: ${request.method} ${request.url}`;
+  console.log(log);
+
+  fs.appendFile("server.log", log + "\n", (err) => {
+    if(err){
+      console.log("unable to append file server.log");
+    }
+  });
+  next();
+});
+
+//set up middleware for maintenance.hbs file.
+//because next() hasnt been called, the page will only display the
+//maintenance text, as the rest of the code is unable to run until next() is called
+// app.use((request, response, next) => {
+//   response.render("maintenance.hbs");
+// });
+
 //make the /public/help.html page available to the server
 //Enter: http://localhost:3000/help.html in the url to view the page
 app.use(express.static(__dirname + "/public"));
 
+//used in the footer.hbs file to dynamically adjust the year in the footer
 hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
 });
 
+//parses the string Welcome to Node Express to upper case. from the app.get function below
 hbs.registerHelper("screamIt", (text) => {
   return text.toUpperCase();
 });
